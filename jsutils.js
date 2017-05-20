bff624878f6f1a6ae9e2b88acc39c75fc56cf452
Zerthox <zerthox@gmail.com>
2017-05-20 15:37:29 +0200

    Fix capitalize

--- jsutils.js
+++ jsutils.js
@@ -9,6 +9,7 @@ Object.defineProperty(String.prototype, "capitalize", {
 	value: function() {
 	    return this.charAt(0).toUpperCase() + this.slice(1);
 	},
+	configurable: true,
 	enumberable: false
 });
 
